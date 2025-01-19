537b60336fe584014012f5b0d34afde340493386
Yaroslav Litvinov <yaroslav.litvinov@pm.me>
2025-01-19 20:51:17 +0000

    feat: add token pbcopy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user_info = request.env['omniauth.auth']
-    render :show, locals: { user_info: user_info }
+    render :show, locals: { user_info: fetch_user_info }
+  end
+
+  private
+
+  def fetch_user_info
+    return request.env['omniauth.auth'] unless Rails.env.development?
+
+    OmniAuth::AuthHash.new(
+      provider: 'developer',
+      credentials: {
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpv
+        aG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+        refresh_token: "NRbtII0kzdptsewzyQAGivCptes=",
+        expires: true,
+        expires_at: (Time.current + 1.day).to_i
+      }
+    )
   end
 end
